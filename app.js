e6291d8edbe5fee73e6883b217aed2117db0c901
Stellanwae <99267699+Stellanwae@users.noreply.github.com>
2024-06-03 16:19:48 +0300

    update vue application

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-
+const app = Vue.createApp({
+    data() {
+      return {
+        listItems: []
+      }
+    },
+    methods: {
+      async fetchData() {
+        try {
+          const res = await fetch("https://jsonplaceholder.typicode.com/posts")
+          const finalRes = await res.json()
+          this.listItems = finalRes
+        } catch (error) {
+          console.error('Error fetching data:', error)
+        }
+      }
+    },
+    mounted() {
+      this.fetchData()
+    }
+  })
+  
+  app.mount('#app')
+  
